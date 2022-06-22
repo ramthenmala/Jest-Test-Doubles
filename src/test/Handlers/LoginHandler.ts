263b67fc62117c94d0fc2df0834708addc1240ef
Ram kumar <webbieram@outlook.com>
2022-06-23 00:27:23 +0530

    added loginHandle test

---
+++ src/test/Handlers/LoginHandler.ts
@@ -0,0 +1,11 @@
+import { LoginHandler } from '../../app/Handlers/LoginHandler'
+
+
+describe('Describe LoginHandler Test Suite', ()=> {
+
+    let loginHandler: LoginHandler
+
+    test('test', ()=> {
+        console.log('Login handler test is working')
+    })
+})
--- src/test/utils/Utils.test.ts
+++ src/test/utils/Utils.test.ts
@@ -1,3 +1,28 @@
+
+import { Utils } from "../../app/Utils/Utils"
+import { IncomingMessage } from "http"
+
 describe('Utils Test Suite', ()=> {
-    
+    test('testRequestPath Valid Path', ()=>{
+        const message = {
+            url: 'http://localhost:8080/login'
+        } as IncomingMessage;
+        const reqPath = Utils.getRequestBasePath(message)
+        expect(reqPath).toBe('login')
+    })
+
+    test('testRequestPath default', () => {
+        const message = {
+            url: 'http://localhost:8080/' 
+        } as IncomingMessage;
+        const reqPath = Utils.getRequestBasePath(message)
+        expect(reqPath).toBeFalsy()
+    })
+    test('testRequestPath null', () => {
+        const message = {
+            url: 'http://localhost:8080/' 
+        } as IncomingMessage;
+        const reqPath = Utils.getRequestBasePath(message)
+        expect(reqPath).toBeFalsy()
+    })
 })
